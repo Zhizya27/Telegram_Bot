31064dfb83cd597e96dbe07f1174c54839651d8c
GrigoryTop <grigory.topychkanov@yandex.ru>
2023-10-17 14:06:21 +0500

    добавлены JavaDOC ко всем классам и методам

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,18 +4,18 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-/** класс, завпускающий бота*/
+/** Class starts bot app */
 public class App {
     public static void main(String[] args) {
 
-        String botName = "forJavaUrfubot";
-        String botToken = "your_token";
+        Config.loading();
         TelegramBotsApi telegramBotsApi = null;
         try {
             telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
-            telegramBotsApi.registerBot(new Bot(botName, botToken));
+            telegramBotsApi.registerBot(new Bot());
         } catch (TelegramApiException e) {
             throw new RuntimeException(e);
+
 
         }
     }
--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -6,44 +6,41 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import Logic.Logic;
-/** класс, реализующий диалог с пользователем
- * и работу с конкретным ботом */
-public class Bot extends TelegramLongPollingBot { //
-    final String botName;
-    final String botToken;
+/** bot constructor class */
+public class Bot extends TelegramLongPollingBot {
 
-    final private Logic botLogic = new Logic();
-
-    public Bot(String botName, String botToken) {
-        this.botName = botName;
-        this.botToken = botToken;
+    Logic botLogic;
+    public Bot(){
+        botLogic = new Logic();
     }
-/** класс реализующий отправку тех же сообщений,
- *  которые присылает пользователь */
+/** method of getting the bot name from the configuration file*/
+    @Override
+    public String getBotUsername () {
+        return Config.botName;
+    }
+/** method of getting the bot token from the configuration file */
+    @Override
+    public String getBotToken () {
+        return Config.botToken;
+    }
+/** a class that implements sending the same messages
+ * that the user sends */
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
             String messageText = update.getMessage().getText();
             long chatId = update.getMessage().getChatId();
             String answer = botLogic.commandHandler(messageText);
-                SendMessage message = new SendMessage();
-                message.setChatId(chatId);
-                message.setText(answer);
-                try {
-                    execute(message);
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
-                }
+            SendMessage message = new SendMessage();
+            message.setChatId(chatId);
+            message.setText(answer);
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
             }
         }
-
-        @Override
-        public String getBotUsername () {
-            return "forJavaUrfubot";
-        }
-
-
-        public String getBotToken () {
-            return "your_token";
-        }
     }
+
+
+}
